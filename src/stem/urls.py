fa9c145762fb515704a210617d4d3735fbdd2d93
Cody Kaup <cody.kaup@getflywheel.com>
2019-02-09 12:32:12 -0600

    Add basic setup of rest framework

--- src/stem/urls.py
+++ src/stem/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from app.views import UserViewSet, UserSerializer
+
+# Rest framework router
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
 ]
