1f7bc6e29e78658b537c97364a476d65af4e905c
Cody Kaup <cody.kaup@getflywheel.com>
2019-02-09 14:01:55 -0600

    Setup PostgreSQL as database

--- src/stem/settings.py
+++ src/stem/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'stem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stem_ecosystem',
+        'USER': 'stem_admin',
+        'PASSWORD': os.environ['STEM_PSQL_PASSWORD'],
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
