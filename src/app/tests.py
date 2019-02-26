117076ae305c85ba47109805096fb257bf7a3d79
Cody Kaup <cody.kaup@getflywheel.com>
2019-02-25 18:56:37 -0600

    Add some model tests for users and events

--- src/app/tests.py
+++ src/app/tests.py
@@ -1,3 +1,54 @@
+from django.db.utils import IntegrityError
 from django.test import TestCase
+from .models.user import User
+from .models.event import Event
 
-# Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(email="student@dundermifflin.com", user_type=1)
+
+    def test_user_student_role(self):
+        user = User.objects.get(email="student@dundermifflin.com")
+        self.assertEqual(user.role().lower(), "student")
+        self.assertTrue(user.is_student)
+
+    def test_user_print_full(self):
+        user = User.objects.get(email="student@dundermifflin.com")
+        user.first_name = "Student"
+        user.last_name = "User"
+        user.username = "studentuser"
+        self.assertEqual(str(user), "Student User (studentuser)")
+
+    def test_user_print(self):
+        user = User.objects.get(email="student@dundermifflin.com")
+        user.username = "studentuser"
+        self.assertEqual(str(user), "studentuser")
+
+class EventTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(username="organizer", email="organizer@dundermifflin.com")
+        Event.objects.create(
+            name="Office Party",
+            date="2019-01-01",
+            event_type=1,
+            organizer=user
+        )
+
+    def test_event_print(self):
+        event = Event.objects.get(name="Office Party")
+        self.assertEqual(str(event), "Office Party")
+
+    def test_event_organizer(self):
+        organizer = User.objects.get(email="organizer@dundermifflin.com")
+        event = Event.objects.get(name="Office Party")
+        self.assertEqual(event.organizer, organizer)
+
+    def test_event_attendees(self):
+        event = Event.objects.get(name="Office Party")
+        attendee_1 = User.objects.create(username="attendee1", email="attendee1@dundermifflin.com")
+        attendee_2 = User.objects.create(username="attendee2", email="attendee2@dundermifflin.com")
+        self.assertEqual(event.attendees.all().count(), 0)
+        event.attendees.add(attendee_1)
+        self.assertEqual(event.attendees.all().count(), 1)
+        event.attendees.add(attendee_2)
+        self.assertEqual(event.attendees.all().count(), 2)
