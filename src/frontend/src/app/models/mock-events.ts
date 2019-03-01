f5a7c0fa220062c8c77b97842a6a3d878ab0c7d9
UNO IST Support <uno-ist-support@unomaha.edu>
2019-02-28 18:54:43 -0600

    Fix some of the card layout problems

--- src/frontend/src/app/models/mock-events.ts
+++ src/frontend/src/app/models/mock-events.ts
@@ -3,7 +3,7 @@ import { Event } from '../models/event';
 export const EVENTS: Event[] = [
   { name: 'Camp 1', date: '08/21/2019', event_type: 'K12/Retreat/Field Trip', organizer: 'Name and/or phone number' },
   { name: 'Program 2', date: '08/21/2019', event_type: 'K12/Retreat/Field Trip', organizer: 'Name and/or phone number' },
-  { name: 'Summer Activity 3', date: '08/21/2019', event_type: 'K12/Retreat/Field Trip', organizer: 'Name and/or phone number' },
+  { name: 'Activity 3', date: '08/21/2019', event_type: 'K12/Retreat/Field Trip', organizer: 'Name and/or phone number' },
   { name: 'Activity 4', date: '08/21/2019', event_type: 'K12/Retreat/Field Trip', organizer: 'Name and/or phone number' },
   { name: 'Activity 5', date: '08/21/2019', event_type: 'K12/Retreat/Field Trip', organizer: 'Name and/or phone number' },
   { name: 'Activity 6', date: '08/21/2019', event_type: 'K12/Retreat/Field Trip', organizer: 'Name and/or phone number' },
