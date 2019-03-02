d13165b5cf48c77b7b438ce052c687766da0bf2e
Cody Kaup <cody.kaup@getflywheel.com>
2019-03-02 12:06:41 -0600

    Add event type converter

--- src/app/models/event.py
+++ src/app/models/event.py
@@ -9,9 +9,12 @@ EVENT_TYPE_CHOICES = (
 class Event(models.Model):
     name = models.CharField(max_length=200)
     date = models.DateField()
-    event_type = models.PositiveSmallIntegerField(choices=EVENT_TYPE_CHOICES, default=1)
+    e_type = models.PositiveSmallIntegerField(choices=EVENT_TYPE_CHOICES, default=1)
     organizer = models.ForeignKey(User, on_delete=models.CASCADE)
     attendees = models.ManyToManyField(User, related_name="events")
 
     def __str__(self):
         return self.name
+
+    def event_type(self):
+        return EVENT_TYPE_CHOICES[self.e_type - 1][1]
