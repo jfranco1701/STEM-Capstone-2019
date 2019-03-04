3d634b3a83481edf203ad3c31cf5f8f83875e8d1
Cody Kaup <dkaup@unomaha.edu>
2019-03-04 11:54:38 -0600

    Update setup for Docker issues with Windows

--- src/stem/settings.py
+++ src/stem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['STEM_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Check to see if this was setup with Docker. If not, we'll want to use 
 # localhost address for the database host
