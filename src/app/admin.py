cacb6a774e12e410f2a15049c20a8517197bd7eb
Cody Kaup <cody.kaup@getflywheel.com>
2019-03-04 22:03:43 -0600

    Add Organization model

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models.user import User
 from .models.event import Event
+from .models.organization import Organization
 
 admin.site.register(User)
 admin.site.register(Event)
+admin.site.register(Organization)
