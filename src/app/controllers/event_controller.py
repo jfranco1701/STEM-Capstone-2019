4c97a0b50cf201b98a0ece652a02d825400b1cc0
Cody Kaup <cody.kaup@getflywheel.com>
2019-03-05 12:35:41 -0600

    Add example API action for future reference

--- src/app/controllers/event_controller.py
+++ src/app/controllers/event_controller.py
@@ -1,5 +1,7 @@
-from django.db import models
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from datetime import date
 
 from app.models.event import Event
 from app.serializers.event_serializer import EventSerializer
@@ -7,3 +9,9 @@ from app.serializers.event_serializer import EventSerializer
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+    @action(detail=False, methods=['get'])
+    def order(self, request, *args, **kwargs):
+        events = Event.objects.filter(date__gte=date.today()).order_by('date')
+        serializer = self.get_serializer(events, many=True)
+        return Response(serializer.data)
