5d002286d9eeceec65e219c9a8e4a197ff3d3a75
Cody Kaup <cody.kaup@getflywheel.com>
2019-03-23 18:49:15 -0500

    Start of event processing

--- src/frontend/src/app/services/event-service.service.ts
+++ src/frontend/src/app/services/event-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { catchError, tap, retry } from 'rxjs/operators';
 import { Event } from '../models/event';
 
 @Injectable({
@@ -8,13 +9,45 @@ import { Event } from '../models/event';
 })
 export class EventService {
   private eventApiUrl: string;
+  private user: any;
 
   constructor(private http: HttpClient) {
     this.eventApiUrl = 'http://localhost:8000/api/v1/events';
+    this.user;
   }
 
   getEvents(): Observable<Event[]> {
-    return this.http.get<Event[]>(this.eventApiUrl);
+    this.user = JSON.parse(localStorage.currentUser);
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.user.token
+      })
+    };
+
+    return this.http.get<Event[]>(this.eventApiUrl, httpOptions);
+    // return this.http.get<Event[]>(this.eventApiUrl);
   }
 
+  addEvent(eventName: string, eventDate: Date, eventType: string) {
+    const obj = {
+      name: eventName,
+      date: eventDate,
+      event_type: eventType
+      // organizer: organizer,
+    };
+
+    var user = JSON.parse(localStorage.currentUser);
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + user.token
+      })
+    };
+
+    return this.http.post<Event>(this.eventApiUrl, obj, httpOptions).pipe(
+      retry(3),
+      tap(_ => console.log('add event')),
+    )
+  }
 }
