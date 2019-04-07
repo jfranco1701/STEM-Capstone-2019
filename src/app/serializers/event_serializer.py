7675bec9004f18eec90089cf86edd1326d1a0a98
Cody Kaup <cody.kaup@getflywheel.com>
2019-04-06 20:33:31 -0500

    Add basic input validation to backend

--- src/app/serializers/event_serializer.py
+++ src/app/serializers/event_serializer.py
@@ -1,6 +1,7 @@
 from app.models.user import User
 from app.models.event import Event
 from rest_framework import serializers
+import bleach
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
     organizer = serializers.HyperlinkedRelatedField(view_name='user-detail', queryset=User.objects.all(), default=serializers.CurrentUserDefault())
@@ -22,3 +23,6 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
     def get_validation_exclusions(self):
         exclusions = super(EventSerializer, self).get_validation_exclusions()
         return exclusions + ['organizer']
+
+    def validate_name(self, value):
+        return bleach.clean(value)
--- src/app/serializers/organization_serializer.py
+++ src/app/serializers/organization_serializer.py
@@ -1,7 +1,10 @@
 from app.models.organization import Organization
 from rest_framework import serializers
+import bleach
 
 class OrganizationSerializer(serializers.HyperlinkedModelSerializer):
+    is_approved = serializers.BooleanField(read_only=True, default=False)
+
     class Meta:
         model = Organization
         fields = (
@@ -9,3 +12,6 @@ class OrganizationSerializer(serializers.HyperlinkedModelSerializer):
             "is_approved",
             "url"
         )
+
+    def validate_name(self, value):
+        return bleach.clean(value)
