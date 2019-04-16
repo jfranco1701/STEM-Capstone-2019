7ad49a01ee64ade3e9f342cc76a72fd63e124e34
Cody Kaup <cody.kaup@getflywheel.com>
2019-04-16 08:48:55 -0500

    Lock user account if we're able to find it on the backend

--- src/app/serializers/jwt_serializer.py
+++ src/app/serializers/jwt_serializer.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import authenticate, user_logged_in
 from rest_framework import serializers
 from rest_framework_jwt.serializers import JSONWebTokenSerializer, jwt_payload_handler, jwt_encode_handler
+from app.models.user import User
 from app.models.userloginactivity import UserLoginActivity
 import datetime
 from django.conf import settings
+from django.utils import timezone
 
 class JWTSerializer(JSONWebTokenSerializer):
     def validate(self, attrs):
@@ -14,11 +16,26 @@ class JWTSerializer(JSONWebTokenSerializer):
 
         if all(credentials.values()):
 
+            user = User.objects.filter(username=attrs.get(self.username_field)).first()
+            if user:
+                if user.account_locked:
+                    msg = 'Account has been locked. Please contact an administrator.'
+                    raise serializers.ValidationError(msg)
+
             if settings.LOCKOUT_ON_FAILURE:
+                user = User.objects.filter(username=attrs.get(self.username_field)).first()
                 failedattempts = UserLoginActivity.objects.filter(login_username=attrs.get(self.username_field), status='F',
-                    login_datetime__gte=datetime.datetime.now() - datetime.timedelta(seconds=settings.LOCKOUT_TIME)).count()
+                    login_datetime__gte=timezone.now() - datetime.timedelta(seconds=settings.LOCKOUT_TIME))
+
+                if user:
+                    failedattempts = failedattempts.filter(login_datetime__gte=user.account_locked_updated_at)
+                    
+                failedattempts = failedattempts.count()
+
                 if failedattempts >= settings.LOCKOUT_MAX_ATTEMPTS -1:
                     msg = 'Too many failed logon attempts.  Account has been locked.'
+                    if user:
+                        user.lock_account()
                     raise serializers.ValidationError(msg)
 
             user = authenticate(request=self.context['request'], **credentials)
