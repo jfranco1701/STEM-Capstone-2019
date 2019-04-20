e4b463306ad54ba5c4fb5da4e24f12dc6ff836bb
Cody Kaup <cody.kaup@getflywheel.com>
2019-04-20 09:08:44 -0500

    Dynamically update debug mode

--- src/stem/settings.py
+++ src/stem/settings.py
@@ -24,7 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['STEM_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if 'DOCKER_PROD_FLAG' in os.environ:
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
