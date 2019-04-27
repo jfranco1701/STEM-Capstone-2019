34a78e6798e220cc28e84434597ce15d91815193
Cody Kaup <cody.kaup@getflywheel.com>
2019-04-27 11:41:25 -0500

    Add event tags to event creation form

--- src/frontend/src/app/components/events/event-add/event-add.component.ts
+++ src/frontend/src/app/components/events/event-add/event-add.component.ts
@@ -4,6 +4,8 @@ import { MatDialog } from '@angular/material';
 import { Router } from '@angular/router';
 import { EventService } from '../../../services/event-service.service';
 import { Event } from 'src/app/models/event';
+import { Tag } from '../../../models/tag';
+import { TagService } from '../../../services/tag.service';
 
 @Component({
   selector: 'app-event-add',
@@ -16,10 +18,12 @@ export class EventAddComponent implements OnInit {
   public errors: any = [];
   event_types = ['Community', 'Camp'];
   event: Event;
+  tags: Tag[];
 
   constructor(
     private fb: FormBuilder, public dialog: MatDialog,
-    private router: Router, private eventService: EventService
+    private router: Router, private eventService: EventService,
+    private tagService: TagService
   ) { }
 
   ngOnInit() {
@@ -29,15 +33,18 @@ export class EventAddComponent implements OnInit {
         name: ['', [Validators.required]],
         date: ['', [Validators.required]],
         e_type: ['', [Validators.required]],
+        tags: ['', ]
       })
     });
+    this.getTags();
   }
 
   onSubmit() {
     this.eventService.addEvent(
       this.eventForm.get('eventGroup').get('name').value,
       this.eventForm.get('eventGroup').get('date').value,
-      this.eventForm.get('eventGroup').get('e_type').value)
+      this.eventForm.get('eventGroup').get('e_type').value,
+      this.eventForm.get('eventGroup').get('tags').value)
       .subscribe(event => {
         this.event = event;
         this.router.navigate(['/home']);
@@ -48,6 +55,13 @@ export class EventAddComponent implements OnInit {
   getErrorMessage(groupName: string, controlName: string) {
     return this.eventForm.get(groupName).get(controlName).hasError('required') ? 'You must enter a value' :
       '';
+  }
+
+  getTags() {
+    this.tagService.getTags()
+      .subscribe(tags => {
+        this.tags = tags;
+      });
   }
 }
 
