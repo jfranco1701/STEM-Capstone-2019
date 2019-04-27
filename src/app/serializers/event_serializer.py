1eddd6bf69b49b32531cefabaab5fe8e3354ade7
Cody Kaup <cody.kaup@getflywheel.com>
2019-04-27 11:41:13 -0500

    Allow events to be created with tags

--- src/app/serializers/event_serializer.py
+++ src/app/serializers/event_serializer.py
@@ -1,12 +1,13 @@
 from app.models.user import User
 from app.models.event import Event
+from app.models.tag import Tag
 from rest_framework import serializers
 import bleach
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
     organizer = serializers.HyperlinkedRelatedField(view_name='user-detail', queryset=User.objects.all(), default=serializers.CurrentUserDefault())
     date = serializers.DateField(format="%m/%d/%Y", input_formats=['%m/%d/%Y', 'iso-8601'])
-    tags = serializers.StringRelatedField(many=True)
+    tags = serializers.SlugRelatedField(queryset=Tag.objects.all(), many=True, required=False, slug_field='name')
 
     class Meta:
         model = Event
